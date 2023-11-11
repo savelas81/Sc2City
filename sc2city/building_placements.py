966bd5da3a835ebad572abad1d5b8f2ba4a3e710
savelas81 <savelas81@gmail.com>
2023-11-11 16:21:52 +0200

    debug drawings

--- sc2city/building_placements.py
+++ sc2city/building_placements.py
@@ -21,6 +21,10 @@ class BuildingPlacementSolver:
         self.supplydepot_positions_priority_3 = []
         self.supplydepot_positions_priority_4 = []
         self.supplydepot_positions_priority_5 = []
+        self.turret_positions_priority_1 = []
+        self.turret_positions_priority_2 = []
+        self.turret_positions_priority_3 = []
+        self.macro_orbitals = []
         self.positions_dict = {}
         self.map_name = ""
         self.start_location_save = Point2((0, 0))
@@ -54,6 +58,14 @@ class BuildingPlacementSolver:
                 self.supplydepot_positions_priority_4.append(building.position)
             elif building.type_id == UnitTypeId.DARKSHRINE:
                 self.supplydepot_positions_priority_5.append(building.position)
+            elif building.type_id == UnitTypeId.MISSILETURRET:
+                self.turret_positions_priority_1.append(building.position)
+            elif building.type_id == UnitTypeId.SPORECRAWLER:
+                self.turret_positions_priority_2.append(building.position)
+            elif building.type_id == UnitTypeId.PHOTONCANNON:
+                self.turret_positions_priority_3.append(building.position)
+            elif building.type_id == UnitTypeId.NEXUS:
+                self.macro_orbitals.append(building.position)
 
         self.positions_dict["building_positions_priority_1"] = self.building_positions_priority_1
         self.positions_dict["building_positions_priority_2"] = self.building_positions_priority_2
@@ -68,6 +80,10 @@ class BuildingPlacementSolver:
         self.positions_dict["supplydepot_positions_priority_3"] = self.supplydepot_positions_priority_3
         self.positions_dict["supplydepot_positions_priority_4"] = self.supplydepot_positions_priority_4
         self.positions_dict["supplydepot_positions_priority_5"] = self.supplydepot_positions_priority_5
+        self.positions_dict["turret_positions_priority_1"] = self.turret_positions_priority_1
+        self.positions_dict["turret_positions_priority_2"] = self.turret_positions_priority_2
+        self.positions_dict["turret_positions_priority_3"] = self.turret_positions_priority_3
+        self.positions_dict["macro_orbitals"] = self.macro_orbitals
         # print(self.positions_dict)
         self.save_data()
 
@@ -104,11 +120,12 @@ class BuildingPlacementSolver:
                 self.supplydepot_positions_priority_3 = self.positions_dict["supplydepot_positions_priority_3"]
                 self.supplydepot_positions_priority_4 = self.positions_dict["supplydepot_positions_priority_4"]
                 self.supplydepot_positions_priority_5 = self.positions_dict["supplydepot_positions_priority_5"]
-                print(self.building_positions_priority_1)
-                print(self.building_positions_priority_2)
-                print(self.building_positions_priority_3)
-                print(self.building_positions_priority_4)
-                print(self.building_positions_priority_5)
+                self.turret_positions_priority_1 = self.positions_dict["turret_positions_priority_1"]
+                self.turret_positions_priority_2 = self.positions_dict["turret_positions_priority_2"]
+                self.turret_positions_priority_3 = self.positions_dict["turret_positions_priority_3"]
+                self.macro_orbitals = self.positions_dict["macro_orbitals"]
+
+
         except (OSError, IOError) as e:
             print("No chat data found.")
             print(e)
@@ -116,42 +133,78 @@ class BuildingPlacementSolver:
     def draw_debug(self):
         red = Point3((255, 0, 0))
         yellow = Point3((255, 255, 0))
+        green = Point3((0, 255, 0))
+        blue = Point3((0, 0, 255))
+        white = Point3((255, 255, 255))
         draw_addon = False
+        """barrack placement debugs below"""
         for p in self.building_positions_priority_1:
             pos = Point2(p)
             size = 1.45
             color = red
             draw_addon = True
-            self.draw_debug_single(pos2=pos, size=size, color=color, draw_addon=draw_addon)
+            self.draw_debug_single(pos2=pos, size=size, height=0.3, color=color, draw_addon=draw_addon)
         for p in self.building_positions_priority_2:
             pos = Point2(p)
             size = 1.45
             color = yellow
             draw_addon = True
-            self.draw_debug_single(pos2=pos, size=size, color=color, draw_addon=draw_addon)
+            self.draw_debug_single(pos2=pos, size=size, height=0.3, color=color, draw_addon=draw_addon)
+
+        """engineeringbay etc placement debug """
+        for p in self.auxiliary_buildings_1:
+            pos = Point2(p)
+            size = 1.45
+            color = red
+            draw_addon = False
+            self.draw_debug_single(pos2=pos, size=size, height=0.3, color=color, draw_addon=draw_addon)
+            self.draw_debug_single(pos2=pos, size=(size - 0.05), height=0.3, color=color, draw_addon=draw_addon)
+
+        """depot placement debug"""
         for p in self.supplydepot_positions_priority_1:
             pos = Point2(p)
             size = 0.95
             color = red
             draw_addon = False
-            self.draw_debug_single(pos2=pos, size=size, color=color, draw_addon=draw_addon)
+            self.draw_debug_single(pos2=pos, size=size, height=0.3, color=color, draw_addon=draw_addon)
         for p in self.supplydepot_positions_priority_2:
             pos = Point2(p)
             size = 0.95
             color = yellow
             draw_addon = False
-            self.draw_debug_single(pos2=pos, size=size, color=color, draw_addon=draw_addon)
+            self.draw_debug_single(pos2=pos, size=size, height=0.3, color=color, draw_addon=draw_addon)
 
-    def draw_debug_single(self, pos2=Point2((0, 0)), size=0.45, color=Point3((255, 255, 255)), draw_addon=False):
+        """turret placement debug"""
+        for p in self.turret_positions_priority_1:
+            pos = Point2(p)
+            size = 0.95
+            color = red
+            draw_addon = False
+            self.draw_debug_single(pos2=pos, size=size, height=0.3, color=color, draw_addon=draw_addon)
+            self.draw_debug_single(pos2=pos, size=(size - 0.05), height=2, color=color, draw_addon=draw_addon)
+
+        """macaro orbital placement debug"""
+        for p in self.macro_orbitals:
+            pos = Point2(p)
+            size = 2.49
+            color = red
+            draw_addon = False
+            self.draw_debug_single(pos2=pos, size=size, height=0.3, color=color, draw_addon=draw_addon)
+
+    def draw_debug_single(self,
+                          pos2=Point2((0, 0)),
+                          size=0.45,
+                          height=0.3,
+                          color=Point3((255, 255, 255)),
+                          draw_addon=False):
         p2 = Point2(pos2)
         h2 = self.ai.get_terrain_z_height(p2)
         pos = Point3((p2.x, p2.y, h2))
-        h = 0.3
-        point0 = Point3((pos.x - size, pos.y - size, pos.z + h))
+        point0 = Point3((pos.x - size, pos.y - size, pos.z + height))
         point1 = Point3((pos.x + size, pos.y + size, pos.z))
         self.ai.client.debug_box_out(point0, point1, color=color)
         if draw_addon:
             pos = Point3((p2.x + 2.5, p2.y - 0.5, h2))
-            point0 = Point3((pos.x - 0.95, pos.y - 0.95, pos.z + h))
+            point0 = Point3((pos.x - 0.95, pos.y - 0.95, pos.z + height))
             point1 = Point3((pos.x + 0.95, pos.y + 0.95, pos.z))
             self.ai.client.debug_box_out(point0, point1, color=color)
