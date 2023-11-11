9406d3cf53dad15ef2fc35b19e4feec5b8200de3
savelas81 <savelas81@gmail.com>
2023-11-11 16:27:57 +0200

    visuals and bugs

--- sc2city/MA_interface.py
+++ sc2city/MA_interface.py
@@ -11,9 +11,9 @@ class MapAnalyserInterface:
         self.debug = True
         self.enemy_ground_grid = None
         self.enemy_air_grid = None
+        self.enemy_ground_to_air_grid = None
         self.reaper_grid = None
         self.map_data = MapData(self.ai, loglevel="INFO")
-
 
     def create_influence_maps(self, memory):
         self.enemy_ground_grid = self.map_data.get_pyastar_grid(default_weight=1)
@@ -29,28 +29,31 @@ class MapAnalyserInterface:
                 self.enemy_ground_grid, self.reaper_grid = self.map_data.add_cost_to_multiple_grids(
                     position=enemy_unit.position,
                     radius=enemy_total_range,
-                    grids=[self.enemy_ground_grid, self.reaper_grid], weight=enemy_unit.ground_dps)
+                    grids=[self.enemy_ground_grid, self.reaper_grid],
+                    weight=enemy_unit.ground_dps)
             if enemy_unit.can_attack_air:
                 enemy_total_range = enemy_unit.radius + enemy_unit.air_range + extra_air_distance
                 if enemy_unit.is_flying:
                     self.enemy_air_grid = self.map_data.add_cost(
                         position=enemy_unit.position,
                         radius=enemy_total_range,
+                        grid=self.enemy_air_grid,
                         weight=enemy_unit.air_dps)
                 else:
                     self.enemy_ground_to_air_grid, self.enemy_air_grid = self.map_data.add_cost_to_multiple_grids(
-                    position=enemy_unit.position,
-                    radius=enemy_total_range,
-                    grids=[self.enemy_ground_to_air_grid, self.enemy_air_grid], weight=enemy_unit.air_dps)
+                        position=enemy_unit.position,
+                        radius=enemy_total_range,
+                        grids=[self.enemy_ground_to_air_grid, self.enemy_air_grid],
+                        weight=enemy_unit.air_dps)
         if self.debug:
-            color: Tuple[int, int, int] = (201, 168, 79)
+            color = Point3((201, 168, 79))
             size: int = 13
             for x, y in zip(*np.where(self.enemy_ground_grid > 1)):
-                    height: float = self.ai.get_terrain_z_height(self.ai.start_location)
-                    pos: Point3 = Point3((x, y, height))
-                    if self.enemy_ground_grid[x, y] == np.inf:
-                        continue
-                    else:
-                        val: int = int(self.enemy_ground_grid[x, y])
-                    self.ai.client.debug_text_world(str(val), pos, color, size)
+                height: float = self.ai.get_terrain_z_height(self.ai.start_location)
+                pos: Point3 = Point3((x, y, height))
+                if self.enemy_ground_grid[x, y] == np.inf:
+                    continue
+                else:
+                    val: int = int(self.enemy_ground_grid[x, y])
+                self.ai.client.debug_text_world(str(val), pos, color, size)
 
