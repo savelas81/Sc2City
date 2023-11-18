0b9fc1f1e3eb45880c8f98a6f976ddf019b75cd0
MarlonVisser <marlonvisser0408@gmail.com>
2023-11-18 17:57:52 +0100

    Updated building placements

--- sc2city/placement_solver.py
+++ sc2city/placement_solver.py
@@ -31,7 +31,7 @@ class BuildingPlacementExtractor(BotAI):
 
 
 def main():
-    mapname = "TESTV10"
+    mapname = "TESTV12"
     opponents = [Race.Protoss, Race.Zerg, Race.Terran]
     opponent = random.choice(opponents)
     # opponent = Race.Terran
