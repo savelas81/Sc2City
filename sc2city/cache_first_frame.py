262fd8446327ce8cdb6fd7d1f8b72b3c43bdba3c
Lucas Huguenin <l-huguenin@hotmail.com>
2023-12-05 13:33:33 -0800

    Major performance improvements to EnemyExpansions

--- sc2city/cache_first_frame.py
+++ sc2city/cache_first_frame.py
@@ -1,9 +1,9 @@
-import math
+from sc2.bot_ai import BotAI
 
 
 class EnemyExpansions:
-    def __init__(self, ai=None):
-        self.ai = ai
+    def __init__(self, bot: BotAI):
+        self.bot = bot
         self.expansions: list = []
         self.natural = None
         self.third = None
@@ -11,33 +11,20 @@ class EnemyExpansions:
         self.fifth = None
 
     async def cache_enemy_expansions(self):
-        expansions: list = self.ai.expansion_locations_list
+        all_expansions = self.bot.expansion_locations_list
+        enemy_start_location = self.bot.enemy_start_locations[0]
+        distances = []
 
-        for t in range(0, 4):
-            closest = None
-            distance = math.inf
-            for el in expansions:
+        for expansion in all_expansions:
+            path_distance = await self.bot.client.query_pathing(
+                enemy_start_location, expansion
+            )
+            if path_distance is not None:
+                distances.append((path_distance, expansion))
 
-                def is_near_to_expansion(t):
-                    return t.distance_to(el) < self.ai.EXPANSION_GAP_THRESHOLD
+        distances.sort(key=lambda x: x[0])
+        self.expansions = [expansion for _, expansion in distances]
 
-                if any(map(is_near_to_expansion, self.ai.townhalls)):
-                    # already taken
-                    continue
-
-                startp = self.ai.enemy_start_locations[0]
-                d = await self.ai.client.query_pathing(startp, el)
-                if d is None:
-                    continue
-
-                if d < distance:
-                    distance = d
-                    closest = el
-
-            self.expansions.append(closest)
-            expansions.remove(closest)
-        print(self.ai.enemy_start_locations[0])
-        print(self.expansions)
         self.natural = self.expansions[0]
         self.third = self.expansions[1]
         self.fourth = self.expansions[2]
